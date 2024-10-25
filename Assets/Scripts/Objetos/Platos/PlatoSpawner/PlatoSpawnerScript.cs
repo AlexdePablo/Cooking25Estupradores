bbe2e0106894a5d82cbfef5ff934c6bc0f55ee4d
Alex <115654119+AlexdePablo@users.noreply.github.com>
2024-10-25 21:19:25 +0200

    Escena Main Hecha

--- Assets/Scripts/Objetos/Platos/PlatoSpawner/PlatoSpawnerScript.cs
+++ Assets/Scripts/Objetos/Platos/PlatoSpawner/PlatoSpawnerScript.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class PlatoSpawnerScript : MonoBehaviour
 {
@@ -15,17 +16,16 @@ public class PlatoSpawnerScript : MonoBehaviour
     private void SpawnItem()
     {
         GameObject ingredient = mPool.GetElement();
-        ingredient.transform.position = transform.position;
+        ingredient.transform.localPosition = new Vector3(Random.Range(-1f, 1f), 3, Random.Range(-1f, 1f));//transform.position;
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        print("Start Spawner");
         for(int i = 0; i < mIngredientsToSpawn; i++)
         {
             GameObject ingredient = mPool.GetElement();
-            ingredient.transform.position = transform.position;
+            ingredient.transform.localPosition = new Vector3(Random.Range(-1f, 1f), 3, Random.Range(-1f, 1f));//transform.position;
         }
     }
 }
